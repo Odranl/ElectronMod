0f5a5a1ec38161490175d5fb5fdb4db2b2c841ca
odran123 <leonardo.zorzikm@gmail.com>
2016-06-27 21:57:31 +0200

    Added Ore generation underground
    The ore willl generate in the cavern layer

---
+++ EMWorld.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.World.Generation;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Generation;
+using System;
+
+namespace ElectronMod
+{
+    public class EMWorld : ModWorld
+    {
+        private const int saveVersion = 0;
+
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            int ShiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
+            if (ShiniesIndex == -1)
+            {
+                // Shinies pass removed by some other mod.
+                return;
+            }
+            tasks.Insert(ShiniesIndex + 1, new PassLegacy("Electron Ores", delegate (GenerationProgress progress)
+            {
+                progress.Message = "Placing Electron Ores";
+
+                for (int i = 0; i < (int)((double)(Main.maxTilesX * Main.maxTilesY) * 0.00014); i++)
+                {
+                    WorldGen.TileRunner(WorldGen.genRand.Next(0, Main.maxTilesX), WorldGen.genRand.Next((int)WorldGen.rockLayer, Main.maxTilesY), (double)WorldGen.genRand.Next(4, 8), WorldGen.genRand.Next(4, 8), mod.TileType("ElectronOre"), false, 0f, 0f, false, true);
+                }
+            })); 
+        }
+
+    }
+}
