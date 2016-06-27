05d89022dc0dbdb7ef1ccfcb653f77ce4b9a830f
odran123 <leonardo.zorzikm@gmail.com>
2016-06-27 19:13:59 +0200

    Added Electron projectile and ore tile

---
+++ Tiles/ElectronOre.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ElectronMod.Tiles
+{
+    public class ElectronOre : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            drop = mod.ItemType("ElectronOre");
+            AddMapEntry(new Color(200, 200, 200));
+            Main.tileSpelunker[Type] = true;
+        }
+    }
+}
